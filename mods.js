df34933f1b2461508300bec56c16fcbef24aef59
Peter Green <fishlegs10@gmail.com>
2020-08-26 17:58:57 -0400

    EXPRESS2

--- mods.js
+++ mods.js
@@ -1,108 +1,78 @@
 const express = require("express");
 const mods = express.Router();
+const pool = require('./pg-connection-pool');
 
-let products = [
+function getTable(req,res){
+    pool.query("select * from shopping_cart order by id").then(result =>
     {
-        id: 1,
-        product: "Tardigrade",
-        price: 1,
-        quantity: 10
-    },
-    {
-        id: 2,
-        product: "Maned Wolf",
-        price: 2,
-        quantity: 9
-    },
-    {
-        id: 3,
-        product: "Sloth",
-        price: 3,
-        quantity: 8
-    },
-    {
-        id: 4,
-        product: "Eagle",
-        price: 4,
-        quantity: 7
-    },
-    {
-        id: 5,
-        product: "Red Panda",
-        price: 5,
-        quantity: 6
-    },
-    {
-        id: 6,
-        product: "Tapir",
-        price: 6,
-        quantity: 5
-    },
-    {
-        id: 7,
-        product: "Aardvark",
-        price: 7,
-        quantity: 4
-    },
-    {
-        id: 8,
-        product: "Goblin Shark",
-        price: 8,
-        quantity: 3
-    },
-    {
-        id: 9,
-        product: "Giant Squid",
-        price: 9,
-        quantity: 2
-    }
-];
+        res.json(result.rows);
+    });
+}
+
 
 mods.get("/", (req, res) => {
     res.sendStatus(200);
 });
 
 mods.get('/cart-items', (req, res) => {
-    res.json({products});
-    res.sendStatus(200);
+    if(!req.query.maxPrice&&!req.query.prefix&&!req.query.pageSize)
+    {
+        getTable(req,res);
+    }else if(req.query.maxPrice)
+    {
+        pool.query("SELECT * FROM shopping_cart where price <= $1::int", [req.query.maxPrice]).then(result =>
+        {
+            res.json(result.rows);
+        });
+    }else if(req.query.prefix)
+    {
+        pool.query("SELECT * FROM shopping_cart WHERE products LIKE $1::text", [req.query.prefix]).then(result =>
+        {
+            res.json(result.rows);
+        });
+    }else
+    {
+        pool.query("SELECT * FROM shopping_cart LIMIT $1::int", [req.query.pageSize]).then(result =>
+        {
+            res.json(result.rows);
+        });
+    }
 })
 
 mods.get("/cart-items/:id", (req, res) => {
-    const product = products.find((item)=>item.id === parseInt(req.params.id));
-    if(product){
-        res.json(product);
-    }else{
-        res.json("ID Not Found")
-        res.sendStatus(404);
-    }
-
+    pool.query("select * from shopping_cart where id=$1::int order by id", [req.params.id]).then(result =>
+    {
+        if(result)
+        {
+            res.status(200).json(result.rows);
+        }else{
+            res.sendStatus(404);
+        }
+    });
 })
+
 
 mods.post("/cart-items", (req, res) => {
-    if(req.body && req.body.product && req.body.price && req.body.quantity)
-    {
-        products.push({product:req.body.product, price:req.body.price, quantity:req.body.quantity, id:products.length+1});
-        res.sendStatus(201);
-    }else
-    {
-        res.json('something broke...');
-    }
+    pool.query("insert into shopping_cart (product, price, quantity) values($1::text, $2::int, $3::int)", [req.body.product, req.body.price, req.body.quantity]).then(result =>
+        {
+            res.status(201).json(result.rows);
+        });
 })
+
 
 mods.put("/cart-items/:id", (req, res) => {
-    const spot = products.findIndex((item)=>item.id === parseInt(req.params.id));
-    const spooki = products.find((item)=>item.id === parseInt(req.params.id));
-    spooki.product = req.body.product;
-    spooki.price = req.body.price;
-    spooki.quantity = req.body.quantity;
-    res.sendStatus(200);
+    pool.query("update shopping_cart set product=$1::text and set price=$2::int and set quantity=$3::int WHERE id=$4::int", [req.body.product, req.body.price, req.body.quantity]).then(result =>
+        {
+            res.status(200).json(result.rows);
+        });
 })
 
-    
+
 mods.delete("/cart-items/:id", (req, res) => {
-    const spot = products.findIndex((item)=>item.id === parseInt(req.params.id));
-    products.splice(spot, 1);
-    res.sendStatus(204);
+    pool.query("delete from shopping_cart  WHERE id=$1::int", [req.query.id]).then(result =>
+        {
+            res.setStatus(200);
+        });
 })
 
 module.exports = mods;
--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 'use strict'
 const express = require('express');
-const route = express.Router();
-const cors = require("cors");
-const app = express();
-const port = 3000;
-app.use(cors());
-app.use(express.json());
-
-
 const mods = require("./mods");
+const cors = require("cors");
+
+const app = express();
+const route = express.Router();
+
+app.use(express.json());
+app.use(express.static(__dirname + "/dist"));
+app.use(cors());
 app.use("/", mods);
 
-
-
-
-
-
+const port = 3000;
 app.listen(port, ()=>{console.log('server listening on port ' + port)});
